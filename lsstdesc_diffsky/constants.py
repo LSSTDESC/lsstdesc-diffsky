495371d2f11cb6a79af29a84daa175acc465142c
Andrew Hearin <ahearin@anl.gov>
2023-04-10 15:05:00 -0500

    Define constants module

---
+++ lsstdesc_diffsky/constants.py
@@ -0,0 +1,4 @@
+"""This module stores globals used throughout the repository."""
+MAH_PNAMES = []
+MS_U_PNAMES = []
+Q_U_PNAMES = []
---
+++ lsstdesc_diffsky/tests/test_constants.py
@@ -0,0 +1,12 @@
+"""
+"""
+from .. import constants
+
+
+def test_diffmah_pnames():
+    assert constants.MAH_PNAMES == 43
+
+
+def test_diffstar_pnames():
+    assert constants.MS_U_PNAMES == 43
+    assert constants.Q_U_PNAMES == 43
