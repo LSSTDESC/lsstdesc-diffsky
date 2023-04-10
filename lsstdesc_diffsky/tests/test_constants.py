63cd2bf85e6a57c03ea2586cc747b41c1a84d366
Eve Kovacs <kovacs@anl.gov>
2023-04-10 23:36:10 +0000

    More pep8

--- lsstdesc_diffsky/tests/test_constants.py
+++ lsstdesc_diffsky/tests/test_constants.py
@@ -13,7 +13,8 @@ def test_diffmah_pnames():
 
 def test_diffstar_pnames():
     assert constants.MS_U_PNAMES == ['diffstar_u_lgmcrit', 'diffstar_u_lgy_at_mcrit',
-                                     'diffstar_u_indx_lo', 'diffstar_u_indx_hi', 'diffstar_u_tau_dep']
+                                     'diffstar_u_indx_lo', 'diffstar_u_indx_hi',
+                                     'diffstar_u_tau_dep']
     assert constants.Q_U_PNAMES == [
         'diffstar_u_qt',
         'diffstar_u_qs',
