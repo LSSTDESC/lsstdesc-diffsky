be0dea94f169402c57c4543066883ea7ba1bc346
Eve Kovacs <kovacs@anl.gov>
2023-04-10 23:08:20 +0000

    pep8 compliance

--- lsstdesc_diffsky/constants.py
+++ lsstdesc_diffsky/constants.py
@@ -1,5 +1,13 @@
 """This module stores globals used throughout the repository."""
-MAH_PNAMES = ['diffmah_logmp_fit', 'diffmah_mah_logtc', 'diffmah_early_index', 'diffmah_late_index']
+MAH_PNAMES = [
+    'diffmah_logmp_fit',
+    'diffmah_mah_logtc',
+    'diffmah_early_index',
+    'diffmah_late_index']
 MS_U_PNAMES = ['diffstar_u_lgmcrit', 'diffstar_u_lgy_at_mcrit',
                'diffstar_u_indx_lo', 'diffstar_u_indx_hi', 'diffstar_u_tau_dep']
-Q_U_PNAMES = ['diffstar_u_qt', 'diffstar_u_qs', 'diffstar_u_q_drop', 'diffstar_u_q_rejuv']
+Q_U_PNAMES = [
+    'diffstar_u_qt',
+    'diffstar_u_qs',
+    'diffstar_u_q_drop',
+    'diffstar_u_q_rejuv']
