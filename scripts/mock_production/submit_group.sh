d1de4b2b8e8d6dfaded9ba150fb1bf1ab65bc81d
Eve Kovacs <kovacs@anl.gov>
2023-05-02 22:18:15 +0000

    update submission script

--- scripts/mock_production/submit_group.sh
+++ scripts/mock_production/submit_group.sh
@@ -18,25 +18,29 @@ fi
 tot_pix_grp=12
 if [ "$hpx_group" == "image" ]
 then
-# 131 pixels in file; 8 pixels per node
-nodes=17
+# 131 pixels in file; 4 pixels per node
+nodes=33
 else
+if [ "$hpx_group" == "test" ]
+then
+nodes=1
 if [ "$hpx_group" -lt "$tot_pix_grp" ]
 then
-# 128 pixels per file; 8 pixels per node
-nodes=16
+# 128 pixels per file; 4 pixels per node
+nodes=32
 else
 if [ "$hpx_group" -gt "$tot_pix_grp" ]
 then
 #special case for reruns
 nodes=1
 else
-# 32 pixels per file; 8 pixels per node                   
-nodes=4
+# special end case: 32 pixels per file; 4 pixels per node                   
+nodes=8
+fi
 fi
 fi
 fi
 
-qsub -n ${nodes} -t 11:00:00 -A LastJourney -M ${EMAIL} ./bundle_skysim_v3_hpx_z.sh ${hpx_group} 0
-qsub -n ${nodes} -t 11:00:00 -A LastJourney -M ${EMAIL} ./bundle_skysim_v3_hpx_z.sh ${hpx_group} 1
-qsub -n ${nodes} -t 11:00:00 -A LastJourneu -M ${EMAIL} ./bundle_skysim_v3_hpx_z.sh ${hpx_group} 2
+qsub -n ${nodes} -t 11:00:00 -A LastJourney -M ${EMAIL} ./bundle_diffsky_hpx_z.sh ${hpx_group} 0
+qsub -n ${nodes} -t 11:00:00 -A LastJourney -M ${EMAIL} ./bundle_diffsky_hpx_z.sh ${hpx_group} 1
+qsub -n ${nodes} -t 11:00:00 -A LastJourneu -M ${EMAIL} ./bundle_diffsky_hpx_z.sh ${hpx_group} 2
