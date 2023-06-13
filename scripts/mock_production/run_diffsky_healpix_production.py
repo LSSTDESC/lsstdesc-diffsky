dbe45e0ff0d5068acb66f4f3634e00e1d0bc8119
Eve Kovacs <kovacs@anl.gov>
2023-06-13 17:55:54 -0500

    Change inout to use only cutout number

--- scripts/mock_production/run_diffsky_healpix_production.py
+++ scripts/mock_production/run_diffsky_healpix_production.py
@@ -41,8 +41,8 @@ sys.path.insert(0, path_to_lsstdesc_diffsky)
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("healpix_fname",
-                    help="Filename of healpix cutout to run")
+parser.add_argument("healpix_number",
+                    help="Healpix cutout number to run")
 # parser.add_argument("commit_hash",
 #    help="Commit hash to save in output files")
 parser.add_argument("-input_master_dirname",
@@ -88,6 +88,7 @@ output_mock_dirname = os.path.join(
     name)
 shape_dir = os.path.join(input_master_dirname, inputs['file_dirs']['shape_dirname'])
 pkldirname = os.path.join(input_master_dirname, inputs['file_dirs']['pkldirname'])
+healpix_fname_template = inputs['file_names']['healpix_fname']
 
 SED_pars = {}
 
@@ -120,7 +121,7 @@ for zdir in z_range_dirs:
                          
     # get list of snapshots
     healpix_cutout_fname = os.path.join(
-        healpix_cutout_dirname, zdir, args.healpix_fname)
+        healpix_cutout_dirname, zdir, healpix_fname_template.format(args.healpix_number))
     print('Processing healpix cutout {}'.format(healpix_cutout_fname))
     healpix_data, redshift_strings, snapshots, z2ts = get_healpix_cutout_info(pkldirname,
                                                                               healpix_cutout_fname, sim_name='AlphaQ')
