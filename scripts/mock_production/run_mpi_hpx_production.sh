d7c4f963358e891725e38c3f4b199e53346164a1
Eve Kovacs <kovacs@anl.gov>
2023-07-22 03:44:39 +0000

    Fix typos

--- scripts/mock_production/run_mpi_hpx_production.sh
+++ scripts/mock_production/run_mpi_hpx_production.sh
@@ -9,13 +9,13 @@ echo "       optional 3rd parameter: name of yaml config file to use"
 echo "       default (diffsky_config) (.yaml is assumed)"
 exit
 else
-hpx_list="pixels_${1}.txt"
+hpx_list="${1}"
 z_range=${2}
 vprod="diffsky_v0.1.0_production"
 cd /lus/eagle/projects/LastJourney/kovacs/Catalog_5000/OR_5000/${vprod}
 echo "Running from `pwd`"
-
-echo "hpx_list=${hpx_list}"
+pixels_list="pixels_${hpx_list}.txt"
+echo "pixels_list=${pixels_list}"
 echo "z_range=${z_range}"
 if [ "$#" -gt 2 ]
 then
@@ -34,16 +34,16 @@ PYTHONPATH=/home/ekovacs/.conda/envs/diffsky/bin/python
 export PYTHONPATH
 
 tot_pix_grp=17
-if [ "$hpx_group" == "image" ]
+if [ "$hpx_list" == "image" ]
 then
 #131 pixels
 total_pix_num=131
 else
-if [[ "${1}" =~ "test" ]]; then
+if [[ "${hpx_list}" =~ "test" ]]; then
 #test# gives number of pixels
-total_pix_num=$(expr "${hpx_group#*test}")
+total_pix_num=$(expr "${hpx_list#*test}")
 else
-if [ "$hpx_group" -lt "$tot_pix_grp" ]
+if [ "$hpx_list" -lt "$tot_pix_grp" ]
 then
 # 128 pixels per file
 total_pix_num=128
@@ -57,6 +57,6 @@ echo "total_pix_num=${total_pix_num}"
 
 script_name=run_diffsky_healpix_production.py
 pythonpath=/home/ekovacs/.conda/envs/diffsky/bin/python
-args="${hpx_list} -zrange_value ${z_range} ${xtra_args}"
+args="${pixels_list} -zrange_value ${z_range} ${xtra_args}"
 
 mpiexec -n ${total_pix_num} ${pythonpath} ${script_name} ${args}
