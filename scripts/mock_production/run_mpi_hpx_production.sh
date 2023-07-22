784138db96e36256438c405b6b480899e941db01
Eve Kovacs <kovacs@anl.gov>
2023-07-22 00:24:44 +0000

    Update pipeline to use mpi4py, but retain ability to run single hpx for testing

---
+++ scripts/mock_production/run_mpi_hpx_production.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+if [ "$#" -lt 2 ]
+then
+echo "Run mpi processing of healpix file list (assign 4 pixels per node)"
+echo "Usage: run_mpi_hpx_production hpx_list (0-17, image, test#) z_range (0-2)"
+echo "       filename for healpix list will be pixels_${1}.txt"
+echo "       optional 3rd parameter: name of yaml config file to use"
+echo "       default (diffsky_config) (.yaml is assumed)"
+exit
+else
+hpx_group=${1}
+z_range=${2}
+vprod="diffsky_v0.1.0_production"
+cd /lus/eagle/projects/LastJourney/kovacs/Catalog_5000/OR_5000/${vprod}
+echo "Running from `pwd`"
+
+echo "hpx_group=pixels_${hpx_group}.txt"
+echo "z_range=${z_range}"
+if [ "$#" -gt 2 ]
+then
+config_file=${3}
+echo "config_file=${config_file}"
+xtra_args="-config_file ${config_file}"
+cname=${config_file}
+else
+xtra_args=""
+cname=${vprod}
+fi
+fi
+
+source activate diffsky
+PYTHONPATH=/home/ekovacs/.conda/envs/diffsky/bin/python
+export PYTHONPATH
+
+tot_pix_grp=17
+if [ "$hpx_group" == "image" ]
+then
+#131 pixels
+total_pix_num=131
+else
+if [[ "${1}" =~ "test" ]]; then
+#test# gives number of pixels
+total_pix_num=$(expr "${hpx_group#*test}")
+else
+if [ "$hpx_group" -lt "$tot_pix_grp" ]
+then
+# 128 pixels per file
+total_pix_num=128
+else
+# 74 pixels in last file
+total_pix_num=74
+fi
+fi
+fi
+echo "total_pix_num=${total_pix_num}"
+
+script_name=run_diffsky_healpix_production.py
+pythonpath=/home/ekovacs/.conda/envs/diffsky/bin/python
+
+mpiexec -n ${total_pix_num} ${pythonpath} ${script_name} ${args}
