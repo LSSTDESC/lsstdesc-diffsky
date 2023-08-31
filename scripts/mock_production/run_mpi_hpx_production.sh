56dc97066ec34c9a38f1673b01d22400f0b77ffd
Eve Kovacs <kovacs@anl.gov>
2023-08-31 11:59:27 -0600

    Add vista filters, fix variable names and minor improvements to submission scripts (#36)
    * Updates to include JHK filters
    
    * Update for new minor version with improved column name, vista fields and roman-rubin production
    
    * Rename pixel lists
    
    * Minor script changes
    
    * Fix linter

--- scripts/mock_production/run_mpi_hpx_production.sh
+++ scripts/mock_production/run_mpi_hpx_production.sh
@@ -11,8 +11,6 @@ exit
 else
 hpx_list="${1}"
 z_range=${2}
-vprod="diffsky_v0.1.0_production"
-cd /lus/eagle/projects/LastJourney/kovacs/Catalog_5000/OR_5000/${vprod}
 echo "Running from `pwd`"
 pixels_list="pixels_${hpx_list}.txt"
 echo "pixels_list=${pixels_list}"
@@ -22,10 +20,8 @@ then
 config_file=${3}
 echo "config_file=${config_file}"
 xtra_args="-config_file ${config_file}"
-cname=${config_file}
 else
 xtra_args=""
-cname=${vprod}
 fi
 fi
 
@@ -33,26 +29,8 @@ source activate diffsky
 PYTHONPATH=/home/ekovacs/.conda/envs/diffsky/bin/python
 export PYTHONPATH
 
-tot_pix_grp=17
-if [ "$hpx_list" == "image" ]
-then
-#131 pixels
-total_pix_num=131
-else
-if [[ "${hpx_list}" =~ "test" ]]; then
-#test# gives number of pixels
-total_pix_num=$(expr "${hpx_list#*test}")
-else
-if [ "$hpx_list" -lt "$tot_pix_grp" ]
-then
-# 128 pixels per file
-total_pix_num=128
-else
-# 74 pixels in last file
-total_pix_num=74
-fi
-fi
-fi
+#retrive number of pixels in list file
+total_pix_num="`wc -l < ${pixels_list}`"
 echo "total_pix_num=${total_pix_num}"
 
 script_name=run_diffsky_healpix_production.py
@@ -60,3 +38,4 @@ pythonpath=/home/ekovacs/.conda/envs/diffsky/bin/python
 args="${pixels_list} -zrange_value ${z_range} ${xtra_args}"
 
 mpiexec -n ${total_pix_num} ${pythonpath} ${script_name} ${args}
+echo "Running ${pythonpath} ${script_name} ${args} on ${total_pix_num} ranks"
