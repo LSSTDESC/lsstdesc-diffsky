0483f6b742f16bf78ab4ff48e8eda3fda7f36119
Andrew Hearin <ahearin@anl.gov>
2023-10-10 13:17:05 -0500

    Add infer_software_versions function

---
+++ lsstdesc_diffsky/tests/test_infer_diffcode_versions.py
@@ -0,0 +1,10 @@
+"""
+"""
+from ..infer_diffcode_versions import infer_software_versions
+
+
+def test_infer_diffcode_versions():
+    versions = infer_software_versions()
+    assert set(list(versions.keys())) == set(
+        ("diffmah", "diffstar", "dsps", "diffsky", "lsstdesc_diffsky")
+    )
