7fb418376a54d20d740cfec60d9dd68cb5541db2
Andrew Hearin <ahearin@anl.gov>
2023-10-24 06:41:44 -0500

    Resolve failing test of software versions

--- lsstdesc_diffsky/tests/test_infer_diffcode_versions.py
+++ lsstdesc_diffsky/tests/test_infer_diffcode_versions.py
@@ -6,5 +6,5 @@ from ..infer_diffcode_versions import infer_software_versions
 def test_infer_diffcode_versions():
     versions = infer_software_versions()
     assert set(list(versions.keys())) == set(
-        ("diffmah", "diffstar", "dsps", "diffsky", "lsstdesc_diffsky")
+        ("diffmah", "diffstar", "dsps", "lsstdesc_diffsky")
     )
