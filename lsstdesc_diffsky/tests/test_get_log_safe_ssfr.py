740f3bf6846f67b7c93316915619916521354318
Eve Kovacs <kovacs@anl.gov>
2023-10-24 19:35:14 +0000

    Bug fix for call to get_log_safe_ssfr, unit test update for versions and new unit test for get_log_safe_ssfr

---
+++ lsstdesc_diffsky/tests/test_get_log_safe_ssfr.py
@@ -0,0 +1,20 @@
+import numpy as np
+from ..photometry.get_SFH_from_params import get_log_safe_ssfr
+
+Ngals = 100
+Mstar = 1e10
+SFR = 1e11
+
+def test_get_log_safe_ssfr():
+    # setup arrays
+    mstar = np.array([Mstar]*Ngals)
+    sfr = np.zeros(Ngals)
+    # replace selected values with finite values
+    sfr[0:int(Ngals/2)] = SFR
+    log_ssfr = get_log_safe_ssfr(mstar, sfr)
+    assert np.all(np.isfinite(log_ssfr))
+    assert log_ssfr.shape == (Ngals,)
+    assert np.all(log_ssfr[0:int(Ngals/2)] == 1)
+    assert np.all(log_ssfr[int(Ngals/2):] < -11)
+
+    return
