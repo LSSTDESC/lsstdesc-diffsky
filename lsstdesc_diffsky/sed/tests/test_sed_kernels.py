96904bd8f6ba3e220124617c8bb4babf6f4636c7
Andrew Hearin <ahearin@anl.gov>
2023-10-26 15:32:55 -0500

    Rewrite sed_kernels

--- lsstdesc_diffsky/sed/tests/test_sed_kernels.py
+++ lsstdesc_diffsky/sed/tests/test_sed_kernels.py
@@ -2,16 +2,8 @@
 """
 import numpy as np
 from diffmah.defaults import DEFAULT_MAH_PARAMS
-from diffsky.experimental.dspspop.boris_dust import (
-    DEFAULT_U_PARAMS as DEFAULT_FUNO_U_PARAMS,
-)
-from diffsky.experimental.dspspop.burstshapepop import DEFAULT_BURSTSHAPE_U_PARAMS
-from diffsky.experimental.dspspop.dust_deltapop import DEFAULT_DUST_DELTA_U_PARAMS
-from diffsky.experimental.dspspop.lgavpop import DEFAULT_LGAV_U_PARAMS
-from diffsky.experimental.dspspop.lgfburstpop import DEFAULT_LGFBURST_U_PARAMS
 from diffstar.defaults import DEFAULT_MS_PARAMS, DEFAULT_Q_PARAMS
 from dsps.data_loaders.retrieve_fake_fsps_data import load_fake_ssp_data
-from dsps.metallicity.defaults import DEFAULT_MET_PARAMS
 
 from ... import read_diffskypop_params
 from ..sed_kernels import calc_rest_sed_galpop, calc_rest_sed_singlegal
@@ -21,21 +13,14 @@ def test_calc_rest_sed_evaluates_with_default_params():
     z_obs = 0.1
 
     ssp_data = load_fake_ssp_data()
+    diffskypop_params = read_diffskypop_params("roman_rubin_2023")
     _res = calc_rest_sed_singlegal(
         z_obs,
         DEFAULT_MAH_PARAMS,
         DEFAULT_MS_PARAMS,
         DEFAULT_Q_PARAMS,
-        ssp_data.ssp_lgmet,
-        ssp_data.ssp_lg_age_gyr,
-        ssp_data.ssp_wave,
-        ssp_data.ssp_flux,
-        DEFAULT_LGFBURST_U_PARAMS,
-        DEFAULT_BURSTSHAPE_U_PARAMS,
-        DEFAULT_LGAV_U_PARAMS,
-        DEFAULT_DUST_DELTA_U_PARAMS,
-        DEFAULT_FUNO_U_PARAMS,
-        DEFAULT_MET_PARAMS,
+        ssp_data,
+        diffskypop_params,
     )
     (
         rest_sed,
@@ -66,11 +51,8 @@ def test_calc_rest_sed_evaluates_with_roman_rubin_2023_params():
         DEFAULT_MAH_PARAMS,
         DEFAULT_MS_PARAMS,
         DEFAULT_Q_PARAMS,
-        ssp_data.ssp_lgmet,
-        ssp_data.ssp_lg_age_gyr,
-        ssp_data.ssp_wave,
-        ssp_data.ssp_flux,
-        *all_params,
+        ssp_data,
+        all_params,
     )
     (
         rest_sed,
@@ -114,8 +96,8 @@ def test_calc_rest_sed_galpop():
         mah_params_galpop,
         ms_params_galpop,
         q_params_galpop,
-        *ssp_data,
-        *all_mock_params,
+        ssp_data,
+        all_mock_params,
     )
     for x in _res:
         assert np.all(np.isfinite(x))
