9dbfe0d189f4e3034884bf6fa4b9a0ff6aca616f
Andrew Hearin <ahearin@anl.gov>
2023-11-03 13:56:10 -0500

    Script is ready except for lsst transmission curves

--- scripts/individual_diffsky_photometry_script.py
+++ scripts/individual_diffsky_photometry_script.py
@@ -4,6 +4,7 @@ import argparse
 import os
 
 import numpy as np
+from astropy.table import Table
 from dsps.data_loaders import load_transmission_curve
 from dsps.data_loaders.defaults import TransmissionCurve
 from galsim import roman
@@ -27,10 +28,10 @@ from lsstdesc_diffsky.photometry.precompute_ssp_tables import (
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("drn", help="directory storing diffsky healpixels")
-    parser.add_argument("ra", help="ra coordinate of diffsky galaxy")
-    parser.add_argument("dec", help="dec coordinate of diffsky galaxy")
-    parser.add_argument("z", help="redshift of diffsky galaxy")
-    parser.add_argument("galid", help="ID of diffsky galaxy")
+    parser.add_argument("ra", help="ra coordinate of diffsky galaxy", type=float)
+    parser.add_argument("dec", help="dec coordinate of diffsky galaxy", type=float)
+    parser.add_argument("z", help="redshift of diffsky galaxy", type=float)
+    parser.add_argument("galid", help="ID of diffsky galaxy", type=int)
     parser.add_argument(
         "-dsps_data_drn",
         help="Directory where dsps data are stored. Default is os.environ['DSPS_DRN']",
@@ -53,7 +54,7 @@ if __name__ == "__main__":
     mock, metadata = load_healpixel(fname)
     msk = mock["galaxy_id"] == galid
     assert msk.sum() == 1, "No galaxy in healpixel with ID = {0}".format(galid)
-    mock = mock[msk]
+    mock = Table(mock)[msk]
 
     lsst_keys_to_print = [key for key in mock.keys() if "LSST" in key.lower()]
     roman_keys_to_print = [key for key in mock.keys() if "Roman" in key.lower()]
@@ -63,17 +64,17 @@ if __name__ == "__main__":
     z_obs = float(mock["redshift"][0])
     zerr_pat = "Input redshift={0} but redshift of galaxy with ID={1} is {2}"
     zerr_msg = zerr_pat.format(z, galid, z_obs)
-    assert np.allclose(z_obs, z, zerr_msg, atol=0.01)
+    assert np.allclose(z_obs, z, atol=0.01), zerr_msg
 
     ra_galid = float(mock["ra"][0])
     ra_pat = "Input ra={0} but ra of galaxy with ID={1} is {2}"
     ra_msg = ra_pat.format(ra, galid, ra_galid)
-    assert np.allclose(ra_galid, ra, ra_msg, atol=0.01)
+    assert np.allclose(ra_galid, ra, atol=0.01), ra_msg
 
     dec_galid = float(mock["dec"][0])
     dec_pat = "Input dec={0} but dec of galaxy with ID={1} is {2}"
     dec_msg = dec_pat.format(dec, galid, dec_galid)
-    assert np.allclose(dec_galid, dec, dec_msg, atol=0.01)
+    assert np.allclose(dec_galid, dec, atol=0.01), dec_msg
 
     diffskypop_params = read_diffskypop_params("roman_rubin_2023")
 
@@ -88,11 +89,17 @@ if __name__ == "__main__":
         for band in roman_filter_list
     ]
 
-    calc_photometry_singlegal(
+    a = [x.wave for x in roman_tcurves]
+    b = [x.transmission for x in roman_tcurves]
+    rest_filter_waves, rest_filter_trans = interpolate_filter_trans_curves(a, b)
+    obs_filter_waves = rest_filter_waves
+    obs_filter_trans = rest_filter_trans
+
+    rest_mags, obs_mags, rest_mags_nodust, obs_mags_nodust = calc_photometry_singlegal(
         z_obs,
-        diffsky_params.mah_params,
-        diffsky_params.ms_params,
-        diffsky_params.q_params,
+        diffsky_params.mah_params[0, :],
+        diffsky_params.ms_params[0, :],
+        diffsky_params.q_params[0, :],
         ssp_data,
         diffskypop_params,
         rest_filter_waves,
