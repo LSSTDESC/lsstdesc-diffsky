c002f1acbad41d49b4ba13af2b39ade89c6c7a3e
Eve Kovacs <kovacs@anl.gov>
2023-11-15 16:35:49 +0000

    improve setup script

--- scripts/mock_production/setup_production_cooley.sh
+++ scripts/mock_production/setup_production_cooley.sh
@@ -12,12 +12,13 @@ mkdir ${catname}
 mkdir ${catname}_production
 
 cd ${catname}_production
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/AlphaQ_z2ts.pkl
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/scripts/mock_production/pixels_${pix_list}.txt
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/scripts/mock_production/run_diffsky_healpix_production.py
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/scripts/mock_production/run_mpi_hpx_production.sh
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/scripts/mock_production/submit_mpi_group.sh
-ln -s /home/ekovacs/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/config_files/${catname}.yaml
+home="/home/ekovacs"
+ln -s ${home}/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/AlphaQ_z2ts.pkl
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/pixels_${pix_list}.txt
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/run_diffsky_healpix_production.py
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/run_mpi_hpx_production.sh
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/submit_mpi_group.sh
+ln -s ${home}/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/config_files/${catname}.yaml
 mkdir cobalt
 mkdir logfiles
 
