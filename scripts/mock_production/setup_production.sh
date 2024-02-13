c18f2ee361a78ca7927e936892221ab2626cfba5
Eve Kovacs <kovacs@anl.gov>
2024-02-13 14:34:00 -0600

    New files for testing with dsps v0.3.7

---
+++ scripts/mock_production/setup_production.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ "$#" -lt 1 ]
+then
+echo "Supply version number eg. 0.3.1"
+exit
+fi
+
+home="~"
+basename="roman_rubin_2023"
+pix_list="test_elais"
+catname=${basename}_v${1}
+mkdir ${catname}
+mkdir ${catname}_production
+
+cd ${catname}_production
+ln -s ${home}/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/AlphaQ_z2ts.pkl
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/pixels_${pix_list}.txt
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/run_diffsky_healpix_production.py
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/run_mpi_hpx_production.sh
+ln -s ${home}/cosmology/lsstdesc-diffsky/scripts/mock_production/submit_mpi_group.sh
+ln -s ${home}/cosmology/lsstdesc-diffsky/lsstdesc_diffsky/config_files/${catname}.yaml
+mkdir mpi
+mkdir logfiles
+
+echo "`pwd`"
+ls -l *
